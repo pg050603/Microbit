578285d315981cd7fdcecc931c613584e3618fa7
Perry Giles <pgil091@UoA.auckland.ac.nz>
2023-05-05 13:53:30 +1200

    Task 3 implementation

--- hwlab_driver.py
+++ hwlab_driver.py
@@ -17,6 +17,7 @@ def clear_display():
     
     TODO: Implement function.
     '''
+    microbit.display.off()
 
 def illuminate_display():
     '''
@@ -24,6 +25,7 @@ def illuminate_display():
     
     TODO: Implement function
     '''
+    machine.mem16[0x50000504] = 0xE000
     
 def display_pixel(row, column):
     '''
@@ -31,6 +33,26 @@ def display_pixel(row, column):
     
     TODO: Implement function
     '''
+    if row == 0:
+        pixels = ['3FE0', '5F70', '3FD0', '5EF0', '3FB0']
+
+    elif row == 1:
+        pixels = ['9F70', '9EF0', '9DF0', '9BF0', '97F0']
+
+    elif row == 2:
+        pixels = ['5FD0', '2FF0', '5FB0', '8FF0', '5FE0']
+
+    elif row == 3:
+        pixels = ['37F0', '3BF0', '3DF0', '3EF0', '3F70']
+
+    elif row == 4:
+        pixels = ['9FB0', '5DF0', '9FE0', '5DF0', '9FD0']
+
+    pixel_on = pixels[column]
+
+    machine.mem16[OUT_address] = pixel_on+str('0x')
+
+
 
 def main():
     '''
