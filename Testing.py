0a886bbdf706baaf845df4142d291b0d0fe1a95f
Perry Giles <pgil091@UoA.auckland.ac.nz>
2023-05-05 15:06:42 +1200

    Completion of Task 3

--- Testing.py
+++ Testing.py
@@ -1,27 +1,9 @@
-def display_pixel(row, column):
-    '''
-    Turn on one specific pixel.
+i = 0
+while i < 5:
+    j = 0
+    while j < 5:
+        print(j)
+        j = j + 1
+    i = i + 1
 
-    TODO: Implement function
-    '''
-    if row == 0:
-        pixels = ['3FE0', '5F70', '3FD0', '5EF0', '3FB0']
 
-    elif row == 1:
-        pixels = ['9F70', '9EF0', '9DF0', '9BF0', '97F0']
-
-    elif row == 2:
-        pixels = ['5FD0', '2FF0', '5FB0', '8FF0', '5FE0']
-
-    elif row == 3:
-        pixels = ['37F0', '3BF0', '3DF0', '3EF0', '3F70']
-
-    elif row == 4:
-        pixels = ['9FB0', '5DF0', '9FE0', '5DF0', '9FD0']
-
-    pixel_on = pixels[column]
-
-    location = str('0x')+pixel_on
-    print(location)
-
-display_pixel(1,1)
--- hwlab_driver.py
+++ hwlab_driver.py
@@ -34,25 +34,23 @@ def display_pixel(row, column):
     TODO: Implement function
     '''
     if row == 0:
-        pixels = ['3FE0', '5F70', '3FD0', '5EF0', '3FB0']
+        pixels = [0x3FE0, 0x5F70, 0x3FD0, 0x5EF0, 0x3FB0]
 
     elif row == 1:
-        pixels = ['9F70', '9EF0', '9DF0', '9BF0', '97F0']
+        pixels = [0x9F70, 0x9EF0, 0x9DF0, 0x9BF0, 0x97F0]
 
     elif row == 2:
-        pixels = ['5FD0', '2FF0', '5FB0', '8FF0', '5FE0']
+        pixels = [0x5FD0, 0x2FF0, 0x5FB0, 0x8FF0, 0x5FE0]
 
     elif row == 3:
-        pixels = ['37F0', '3BF0', '3DF0', '3EF0', '3F70']
+        pixels = [0x37F0, 0x3BF0, 0x3DF0, 0x3EF0, 0x3F70]
 
     elif row == 4:
-        pixels = ['9FB0', '5DF0', '9FE0', '5DF0', '9FD0']
+        pixels = [0x9FB0, 0x5BF0, 0x9FE0, 0x5DF0, 0x9FD0]
 
     pixel_on = pixels[column]
 
-    machine.mem16[OUT_address] = pixel_on+str('0x')
-
-
+    machine.mem16[OUT_address] = pixel_on
 
 def main():
     '''
@@ -67,6 +65,23 @@ def main():
     TODO: clear display, wait 1 second, display centre pixel, wait 1 second,
           turn on all pixels, wait 1 second, clear display
     '''
-
+    #Test 1
+    clear_display()
+    microbit.sleep(1000)
+    display_pixel(2,2)
+    microbit.sleep(1000)
+    clear_display()
+    #Test 2
+    illuminate_display()
+    microbit.sleep(1000)
+    clear_display()
+    i = 0
+    while i < 5:
+        j = 0
+        while j < 5:
+            display_pixel(i, j)
+            microbit.sleep(1000)
+            j = j + 1
+        i = i + 1
 if __name__ == "__main__":
     main()
